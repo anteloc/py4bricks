2d2ff10e034a470d9cd48237adb0e8a5d25a160a
anteloc <6914284+anteloc@users.noreply.github.com>
2026-04-18 00:15:58 +0200

    WIP added attach and place_on_top to pieces methods

--- examples/attach_place_demo.py
+++ examples/attach_place_demo.py
@@ -7,17 +7,17 @@ from py4bricks.geometry import Identity, XAxis, YAxis, ZAxis
 central_piece = Piece(part=Brick1X1, colour=White)
 central_piece.rotation = Identity().rotate(49, axis=YAxis)
 
-north_piece = Piece(part=Brick1X1, colour=Red)
-south_piece = Piece(part=Brick1X1, colour=Green)
-east_piece = Piece(part=Brick1X1, colour=Blue)
-west_piece = Piece(part=Brick1X1, colour=Yellow)
+front_piece = Piece(part=Brick1X1, colour=Red)
+back_piece = Piece(part=Brick1X1, colour=Green)
+right_piece = Piece(part=Brick1X1, colour=Blue)
+top_piece = Piece(part=Brick1X1, colour=Yellow)
 
-Piece.attach(north_piece, to=central_piece, side="front")
-Piece.attach(south_piece, to=central_piece, side="back")
-Piece.attach(east_piece, to=central_piece, side="right")
-# Piece.attach(west_piece, to=central_piece, side="left")
-Piece.place_on_top(piece=west_piece, of=central_piece)
-reprs = [repr(central_piece), repr(north_piece), repr(south_piece), repr(east_piece), repr(west_piece)]
+Piece.attach(front_piece, to=central_piece, side="front")
+Piece.attach(back_piece, to=central_piece, side="back")
+Piece.attach(right_piece, to=central_piece, side="right")
+# Piece.attach(left_piece, to=central_piece, side="left")
+Piece.place_on_top(piece=top_piece, of=central_piece)
+reprs = [repr(central_piece), repr(front_piece), repr(back_piece), repr(right_piece), repr(top_piece)]
 
 Path(Path(__file__).parent / "attach_place_demo.mpd").write_text("\n".join(reprs))
 
